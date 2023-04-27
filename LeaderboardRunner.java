b04353b4af32d7a588233cdf4a699b314066f2ae
APCSAemily <89477158+APCSAemily@users.noreply.github.com>
2023-04-27 13:36:59 -0500

    Create LeaderboardRunner.java

---
+++ LeaderboardRunner.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class LeaderboardRunner {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		LeaderboardFrame frame = new LeaderboardFrame("Standings");
+	}
+
+}
