fe0221ceef8ad6b70c3c9d4bb92305f02b7cced8
APCSAemily <89477158+APCSAemily@users.noreply.github.com>
2023-04-27 13:36:36 -0500

    Create LeaderboardPanel.java

---
+++ LeaderboardPanel.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+import java.util.*;
+import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.*;
+import javax.imageio.*;
+
+public class LeaderboardPanel extends JPanel{
+	
+	public static BufferedImage leaderboardImg;
+	public static int WIDTH = 1600;
+	public static int HEIGHT = 960;
+	
+		
+	
+	public LeaderboardPanel() {
+		
+	}
+	
+	public void paint(Graphics g) {
+		
+	}
+	
+	public void displayLeaderboard(Graphics g) {
+
+		
+	}
+	
+	
+		
+}
