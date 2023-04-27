7b0a6020839a247872348038614850cd87cb5bfc
APCSAemily <89477158+APCSAemily@users.noreply.github.com>
2023-04-27 13:35:17 -0500

    Create StartScreenRunner.java

---
+++ StartScreenRunner.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class StartScreenRunner {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		StartFrame frame = new StartFrame("Start");
+	}
+}
